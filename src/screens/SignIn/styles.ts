948927be5d8dd9d1d90e46d6f8b13da8bd17e3c5
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-06-30 16:03:23 -0300

    Styles Screens 💯

---
+++ src/screens/SignIn/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        backgroundColor: "green",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    textInput: {
+        height: 50,
+        width: 200,
+        borderBottomWidth: 2
+    }
+});
