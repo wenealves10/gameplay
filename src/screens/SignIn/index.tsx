095c4230ed76a4b64364364e8609f2cb1fe4c3f0
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-07-01 22:22:48 -0300

    commit narive 👍

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Image, StatusBar} from "react-native";
+import { View, Text, Image} from "react-native";
 import ImageBackground from "../../assets/illustration.png"
 
 import { styles } from "./styles";
@@ -9,11 +9,6 @@ export function SignIn(){
 
     return (
         <View style={styles.container}>
-          <StatusBar
-           barStyle="light-content"
-           backgroundColor="transparent"
-           translucent
-          />
           <Image 
           source={ImageBackground}
           style={styles.image}
@@ -22,9 +17,9 @@ export function SignIn(){
 
         <View style={styles.content}>
           <Text style={styles.title}>
-            Organize {`\n`}
-            Suas Jogatinas {`\n`}
-            facilmente
+            Conecte-se {`\n`}
+            E organize suas{`\n`}
+            Jogatinas
           </Text>
           <Text style={styles.subtitle}>
           Crie grupos para jogar seus games {`\n`}
