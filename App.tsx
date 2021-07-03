02b3844c7a5ec2777ef977183183937878b2ee08
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-07-03 10:17:43 -0300

    Deloy native sigin

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { useFonts } from "expo-font";
 import { Inter_400Regular,Inter_500Medium} from "@expo-google-fonts/inter";
 import { Rajdhani_500Medium, Rajdhani_700Bold } from "@expo-google-fonts/rajdhani";
 import AppLoading from 'expo-app-loading';
-// import { SignIn } from "./src/screens/SignIn";
+import { SignIn } from "./src/screens/SignIn";
 import { Home } from "./src/screens/Home";
 import { Background } from "./src/components/Background";
 
@@ -27,7 +27,7 @@ export default function App(){
           backgroundColor="transparent"
           translucent
       />
-    <Home/>
+    <SignIn/>
     </Background>
   );
 }
